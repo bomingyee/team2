6b413f8b93f7c9eb45eefcaf74b48cee26f4c4c2
iolive-ui <okin1127@naver.com>
2026-09-11 16:12:09 +0900

    feat. 커피내기 기능 추가

--- coffee.py
+++ coffee.py
@@ -1,6 +1,34 @@
+import random
+
+
 class Coffee():
     def __init__(self):
         pass
 
     def run(self):
-        pass
+        print()
+        print("===================")
+        print("===== 커피내기 =====")
+        print("===================")
+
+        people = []
+
+        count = int(input("참여 인원 수 : "))
+
+        for i in range(count):
+            name = input(str(i + 1) + "번 : ")
+            people.append(name)
+
+        winner = random.choice(people)
+
+        print()
+
+        for i in range(4):
+            print("두구두구...")
+
+        print()
+        print("커피 당첨자는...")
+        print()
+        print(winner)
+        print()
+        print("오늘 커피는", winner + "님이 쏩니다!!!!!")
