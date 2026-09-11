32a7258092d816c848193210a3374a9316327816
iolive-ui <okin1127@naver.com>
2026-09-11 16:33:54 +0900

    feat: 커피내기 기능 보완

--- coffee.py
+++ coffee.py
@@ -22,19 +22,29 @@ class Coffee():
 
         winner = random.choice(people)
 
+        messages = [
+            "오늘의 커피 요정으로 선정되었습니다!",
+            "동료들의 카페인을 책임져 주세요!",
+            "카드는 가볍게, 마음은 무겁게...",
+            "축하합니다! 오늘의 커피 담당입니다!",
+            "다음 기회에는 꼭 피하시길 바랍니다!"
+        ]
+
+        message = random.choice(messages)
+
         print()
 
         for i in range(4):
             print("두구두구...")
-            print("3")
             time.sleep(1)
-            print("2")
-            time.sleep(1)
-            print("1")
-            
+
         print()
         print("커피 당첨자는...")
         print()
         print(winner)
         print()
         print("오늘 커피는", winner + "님이 쏩니다!!!!!")
+
+        print()
+        print("☕ 오늘의 한마디")
+        print(message)
