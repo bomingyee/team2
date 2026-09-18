80b904ed33cd78865137f2cfd81ce7e0ec9d2a15
bomingyee <bomingyee@gmail.com>
2026-09-18 15:48:12 +0900

    app.py 수정 - 기능 연동

--- db.py
+++ db.py
@@ -10,66 +10,67 @@ key = os.environ["SUPABASE_KEY"]
 
 supabase: Client = create_client(url, key)
 
-# 전체 데이터 적재
-def save_data(user_info, order_info):
+class DB():
+    # 전체 데이터 적재
+    def save_data(user_info, order_info):
 
-    # user_info
-    # [user_id, user_name, gender, age]
+        # user_info
+        # [user_id, user_name, gender, age]
 
-    # order_info
-    # [user_id, order_time, menu_id, quantity, price]
+        # order_info
+        # [user_id, order_time, menu_id, quantity, price]
 
-    # 1. users 테이블 적재
-    user_data = {
-        "user_name": user_info[1],
-        "gender": user_info[2],
-        "age": user_info[3]
-    }
+        # 1. users 테이블 적재
+        user_data = {
+            "user_name": user_info[1],
+            "gender": user_info[2],
+            "age": user_info[3]
+        }
 
-    user_result = (
-        supabase
-        .schema("cafe_project")
-        .table("users")
-        .insert(user_data)
-        .select("user_id")
-        .execute()
-    )
+        user_result = (
+            supabase
+            .schema("cafe_project")
+            .table("users")
+            .insert(user_data)
+            .select("user_id")
+            .execute()
+        )
 
-    # DB에서 자동 생성된 user_id
-    user_id = user_result.data[0]["user_id"]
+        # DB에서 자동 생성된 user_id
+        user_id = user_result.data[0]["user_id"]
 
-    # 2. orders 테이블 적재
-    order_data = {
-        "user_id": user_id,
-        "order_time": order_info[1]
-    }
+        # 2. orders 테이블 적재
+        order_data = {
+            "user_id": user_id,
+            "order_time": order_info[1]
+        }
 
-    order_result = (
-        supabase
-        .schema("cafe_project")
-        .table("orders")
-        .insert(order_data)
-        .select("order_id")
-        .execute()
-    )
+        order_result = (
+            supabase
+            .schema("cafe_project")
+            .table("orders")
+            .insert(order_data)
+            .select("order_id")
+            .execute()
+        )
 
-    # DB에서 자동 생성된 order_id
-    order_id = order_result.data[0]["order_id"]
+        # DB에서 자동 생성된 order_id
+        order_id = order_result.data[0]["order_id"]
 
-    # 3. order_items 테이블 적재
-    item_data = {
-        "order_id": order_id,
-        "menu_id": order_info[2],
-        "quantity": order_info[3],
-        "menu_price": order_info[4]
-    }
+        # 3. order_items 테이블 적재
+        item_data = {
+            "order_id": order_id,
+            "menu_id": order_info[2],
+            "quantity": order_info[3],
+            "menu_price": order_info[4]
+        }
 
-    item_result = (
-        supabase
-        .schema("starbucks")
-        .table("order_items")
-        .insert(item_data)
-        .execute()
-    )
+        item_result = (
+            supabase
+            .schema("starbucks")
+            .table("order_items")
+            .insert(item_data)
+            .execute()
+        )
 
-    return [user_id, order_id]
+        return [user_id, order_id]
--- user.py
+++ user.py
@@ -1,6 +1,18 @@
 class User():
-    def __init__(self):
-        pass
+    def __init__(self) : 
+            self.user_id = 0 
+            self.users = []
+    
+    def get_user_info(self) :  
+        self.user_id += 1 
 
-    def get_user_info():
-        pass
+        print("\n고객 정보를 입력합니다.")
+        self.user_name= input("이름을 기재해주세요 : ")
+        self.gender = input("성별을 입력해주세요(남성 혹은 여성으로 입력) : ")
+        self.age = int(input("나이대를 입력해주세요(10, 20, 30, 40, 50, 60, 70, 80, 90, 100) : "))
+
+        print(f"고객님의 ID는 {self.user_id}번 입니다. ")
+        
+        self.users = [self.user_id , self.user_name, self.gender , self.age ] 
+        
+        return self.users
