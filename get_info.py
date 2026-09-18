99884c45d1214885fcbe998114b50832adb7dc01
ksmile2014122-ux <ksmile2014122@gmail.com>
2026-09-18 16:02:34 +0900

    최신 main pull 받기 전 임시 저장

--- get_info.py
+++ get_info.py
@@ -9,9 +9,9 @@ class GetInfo :
         self.user_id += 1 
                 
 
-        print("고객 정보를 입력합니다. : ")
+        print("고객 정보를 입력합니다. : ")  # 제대로 된 값 
         self.user_name= input("이름을 기재해주세요 : ")
-        self.gender = input("성별을 입력해주세요(남성 혹은 여성으로 입력) : ")
+        self.gender = input("성별을 입력해주세요(남성 혹은 여성으로 입력) : ") ## 남성 여성 예외처리
         self.age = int(input("나이대를 입력해주세요(10, 20, 30, 40, 50, 60, 70, 80, 90, 100) : "))
 
         print(f"고객님의 ID는 {self.user_id}번 입니다. ")
